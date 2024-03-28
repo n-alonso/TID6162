52dc99fa0064585e92deaa8f415fd6c52169df65
n-alonso <nmalonsojuan@gmail.com>
2024-03-28 12:18:12 +0200

    fix: commedted out unused plugins

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,24 +1,24 @@
 export default ({ env }) => ({
-  graphql: {
-    config: {
-      endpoint: "/graphql",
-      shadowCRUD: true,
-      playgroundAlways: true,
-      depthLimit: 15,
-      amountLimit: 100,
-      apolloServer: {
-        tracing: false,
-      },
-    },
-  },
-  "shopify-fields": {
-    enabled: true,
-    config: {
-      apiVersion: "2022-07", // Or one of Shopify's supported API versions
-      accessToken: process.env.SHOPIFY_ACCESS_TOKEN, // The environment variable containing your private app's access token
-      shopName: process.env.SHOPIFY_STORE_DOMAIN, // The environment variable containing your myshopify.com domain
-    },
-  },
+  // graphql: {
+  //   config: {
+  //     endpoint: "/graphql",
+  //     shadowCRUD: true,
+  //     playgroundAlways: true,
+  //     depthLimit: 15,
+  //     amountLimit: 100,
+  //     apolloServer: {
+  //       tracing: false,
+  //     },
+  //   },
+  // },
+  // "shopify-fields": {
+  //   enabled: true,
+  //   config: {
+  //     apiVersion: "2022-07", // Or one of Shopify's supported API versions
+  //     accessToken: process.env.SHOPIFY_ACCESS_TOKEN, // The environment variable containing your private app's access token
+  //     shopName: process.env.SHOPIFY_STORE_DOMAIN, // The environment variable containing your myshopify.com domain
+  //   },
+  // },
   upload: {
     config: {
       breakpoints: {

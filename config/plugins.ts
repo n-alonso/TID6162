b0bb2c61dd7d39a39612227ed8b28b98ca9276e9
n-alonso <nmalonsojuan@gmail.com>
2024-03-29 10:00:38 +0200

    enabled shopify plugin

--- config/plugins.ts
+++ config/plugins.ts
@@ -11,14 +11,14 @@ export default ({ env }) => ({
   //     },
   //   },
   // },
-  // "shopify-fields": {
-  //   enabled: true,
-  //   config: {
-  //     apiVersion: "2022-07", // Or one of Shopify's supported API versions
-  //     accessToken: process.env.SHOPIFY_ACCESS_TOKEN, // The environment variable containing your private app's access token
-  //     shopName: process.env.SHOPIFY_STORE_DOMAIN, // The environment variable containing your myshopify.com domain
-  //   },
-  // },
+  "shopify-fields": {
+    enabled: true,
+    config: {
+      apiVersion: "2022-07", // Or one of Shopify's supported API versions
+      accessToken: process.env.SHOPIFY_ACCESS_TOKEN, // The environment variable containing your private app's access token
+      shopName: process.env.SHOPIFY_STORE_DOMAIN, // The environment variable containing your myshopify.com domain
+    },
+  },
   upload: {
     config: {
       breakpoints: {
